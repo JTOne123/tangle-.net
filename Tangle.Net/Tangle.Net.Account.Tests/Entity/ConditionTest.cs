dc020af743a4a77b00bd8111d86b2dae235f426b
Felandil <s.fuhrhop@hotmail.de>
2019-02-06 17:35:42 +0100

    add magnet link parser | add in memory account skeleton

--- Tangle.Net/Tangle.Net.Account.Tests/Entity/ConditionTest.cs
+++ Tangle.Net/Tangle.Net.Account.Tests/Entity/ConditionTest.cs
@@ -21,5 +21,27 @@
         "iota://SSFQNEKDAMMAJSTMLRPIHSKZMHQTMYITPPLUWLOPYKS9K9YDGJZKTNQHJVD9YGZFOVZKAZHDIDMFWJGUYFZOTSAS9C/?t=1576152732&m=false&am=10",
         condition.ToMagnetLink());
     }
+
+    [TestMethod]
+    public void TestMagnetLinkParsing()
+    {
+      var condition = Condition.FromMagnetLink(
+        new Uri("iota://SSFQNEKDAMMAJSTMLRPIHSKZMHQTMYITPPLUWLOPYKS9K9YDGJZKTNQHJVD9YGZFOVZKAZHDIDMFWJGUYFZOTSAS9C/?t=1576152732&m=false&am=10"));
+
+      Assert.AreEqual("SSFQNEKDAMMAJSTMLRPIHSKZMHQTMYITPPLUWLOPYKS9K9YDGJZKTNQHJVD9YGZFOVZKAZHDIDMFWJGUYFZOTSAS9C", condition.Address.Value + condition.Address.Checksum.Value);
+      Assert.AreEqual(new DateTime(2019, 12, 12, 12, 12, 12), condition.TimeoutAt);
+      Assert.IsFalse(condition.MultiUse);
+      Assert.AreEqual(10, condition.ExpectedAmount);
+    }
+
+    [DataTestMethod]
+    [DataRow("http://SSFQNEKDAMMAJSTMLRPIHSKZMHQTMYITPPLUWLOPYKS9K9YDGJZKTNQHJVD9YGZFOVZKAZHDIDMFWJGUYFZOTSAS9C/?t=1576152732&m=false&am=10")]
+    [DataRow("iota://SSFQNEKDAMMAJSTMLRPIHSKZMHQTMYITPPLUWLOPYKS9K9YDGJZKTNQHJVD9YGZFOVZKAZHDIDMFWJGUY/?t=1576152732&m=false&am=10")]
+    [DataRow("iota://SSFQNEKDAMMAJSTMLRPIHSKZMHQTMYITPPLUWLOPYKS9K9YDGJZKTNQHJVD9YGZFOVZKAZHDIDMFWJGUY/")]
+    [ExpectedException(typeof(Exception))]
+    public void TestInvalidMagnetLinkParsing(string magnetLink)
+    {
+      var condition = Condition.FromMagnetLink(new Uri(magnetLink));
+    }
   }
 }
--- Tangle.Net/Tangle.Net.Account/Entity/Condition.cs
+++ Tangle.Net/Tangle.Net.Account/Entity/Condition.cs
@@ -1,6 +1,7 @@
 ﻿namespace Tangle.Net.Account.Entity
 {
   using System;
+  using System.Web;
 
   using Tangle.Net.Entity;
 
@@ -12,13 +13,44 @@
       this.TimeoutAt = timeoutAt;
     }
 
-    public Address Address { get; set; }
+    public Address Address { get; }
+
+    public static Condition FromMagnetLink(Uri uri)
+    {
+      if (uri.Scheme != "iota")
+      {
+        throw new Exception($"Invalid Uri scheme {uri.Scheme}. Only scheme accepted is 'iota://'");
+      }
+
+      if (uri.Host.Length != 90)
+      {
+        throw new Exception("Invalid address length. Please provide the address with checksum (length 90 trytes)");
+      }
+
+      var queryParameters = HttpUtility.ParseQueryString(uri.Query);
+
+      if (string.IsNullOrEmpty(queryParameters.Get("t")) || string.IsNullOrEmpty(queryParameters.Get("m"))
+                                                         || string.IsNullOrEmpty(queryParameters.Get("am")))
+      {
+        throw new Exception("Invalid query parameters. Please provide t (timeoutAt), m (MultiUse) and am (ExpectedAmount)");
+      }
+
+      return new Condition(new Address(uri.Host.ToUpper()), UnixTimeStampToDateTime(int.Parse(queryParameters.Get("t"))))
+               {
+                 ExpectedAmount = long.Parse(queryParameters.Get("am")), MultiUse = bool.Parse(queryParameters.Get("m"))
+               };
+    }
 
     public string ToMagnetLink()
     {
       var unixTimestamp = (int)this.TimeoutAt.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
       return
         $"iota://{this.Address.WithChecksum().Value}{this.Address.Checksum.Value}/?t={unixTimestamp}&m={this.MultiUse.ToString().ToLower()}&am={this.ExpectedAmount}";
+    }
+
+    private static DateTime UnixTimeStampToDateTime(int unixTimeStamp)
+    {
+      return new DateTime(1970, 1, 1).AddSeconds(unixTimeStamp);
     }
   }
 }
