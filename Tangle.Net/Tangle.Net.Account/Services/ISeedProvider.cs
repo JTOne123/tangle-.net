1b7d2e2286cd852e0b874dc0f5be5c4c42c74929
Felandil <s.fuhrhop@hotmail.de>
2019-02-06 13:06:43 +0100

    implement memory solution for seed provider

--- Tangle.Net/Tangle.Net.Account/Services/ISeedProvider.cs
+++ Tangle.Net/Tangle.Net.Account/Services/ISeedProvider.cs
@@ -1,6 +1,9 @@
 ﻿namespace Tangle.Net.Account.Services
 {
+  using Tangle.Net.Entity;
+
   public interface ISeedProvider
   {
+    Seed Seed { get; }
   }
 }
---
+++ Tangle.Net/Tangle.Net.Account/Services/Memory/InMemorySeedProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace Tangle.Net.Account.Services.Memory
+{
+  using Tangle.Net.Entity;
+
+  public class InMemorySeedProvider : ISeedProvider
+  {
+    public InMemorySeedProvider(Seed seed)
+    {
+      this.Seed = seed;
+    }
+
+    /// <inheritdoc />
+    public Seed Seed { get; }
+  }
+}
