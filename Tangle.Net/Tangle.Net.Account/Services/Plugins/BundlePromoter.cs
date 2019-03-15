d1a16c3018f173af8dd9850e4535a3addd21bd20
Felandil <s.fuhrhop@hotmail.de>
2019-03-15 14:28:06 +0100

    use library for promotion

--- Tangle.Net/Tangle.Net.Account/Services/Plugins/BundlePromoter.cs
+++ Tangle.Net/Tangle.Net.Account/Services/Plugins/BundlePromoter.cs
@@ -1,18 +1,50 @@
 ﻿namespace Tangle.Net.Account.Services.Plugins
 {
+  using System;
+  using System.Threading;
+  using System.Threading.Tasks;
+
   public class BundlePromoter : IAccountPlugin
   {
+    private const int PromotionIntervalMilliseconds = 20000;
+
+    private CancellationTokenSource taskTokenSource;
+
     /// <inheritdoc />
     public string Name => "BundlePromoter";
 
     /// <inheritdoc />
     public void Shutdown()
     {
+      this.taskTokenSource.Cancel();
     }
 
     /// <inheritdoc />
     public void Start(IAccount account)
     {
+      this.taskTokenSource = new CancellationTokenSource();
+      Task.Factory.StartNew(() => Promote(account), this.taskTokenSource.Token);
+    }
+
+    private static void Promote(IAccount account)
+    {
+      while (true)
+      {
+        var pendingTransfers = account.Settings.Store.GetPendingTransfers(account.Id);
+
+        foreach (var transfer in pendingTransfers)
+        {
+          try
+          {
+            account.Settings.IotaRepository.PromoteTransactionAsync(transfer.Tail, account.Settings.Depth, account.Settings.MinimumWeightMagnitude).Wait();
+          }
+          catch (ArgumentException)
+          {
+          }
+        }
+
+        Thread.Sleep(PromotionIntervalMilliseconds);
+      }
     }
   }
 }
